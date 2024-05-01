c3c021c96c1b6d1141850d6e3edc3ac163d031cc
Jose Lima <jalimag0206@gmail.com>
2024-05-01 18:16:33 -0300

    style: adjust auth layout

--- src/pages/_layouts/auth.tsx
+++ src/pages/_layouts/auth.tsx
@@ -10,11 +10,11 @@ export const AuthLayout = () => {
           <span className="font-semibold">pizza shop</span>
         </div>
         <footer className="text-sm">
-          Painel do parceiro &copy; pizzar.shop - {new Date().getFullYear()}
+          Painel do parceiro &copy; pizza.shop - {new Date().getFullYear()}
         </footer>
       </div>
 
-      <div className="flex flex-col items-center justify-center">
+      <div className="relative flex flex-col items-center justify-center">
         <Outlet />
       </div>
     </div>
