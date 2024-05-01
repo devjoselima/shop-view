adc4178d01ff5e79a0e4018a4e55144d7b4c846a
Jose Lima <jalimag0206@gmail.com>
2024-05-01 19:17:08 -0300

    feat: add Header

--- src/pages/_layouts/appLayout/components/header.tsx
+++ src/pages/_layouts/appLayout/components/header.tsx
@@ -1,7 +1,29 @@
+import { Home, Pizza, UtensilsCrossed } from 'lucide-react'
+
+import { Separator } from '@/components/ui'
+
+import { NavLink } from './nav-link'
+
 export const Header = () => {
   return (
-    <header>
-      <h1>Header</h1>
+    <header className="border-b">
+      <div className="flex h-16 items-center gap-6 px-6">
+        <Pizza className="h-6 w-6" />
+
+        <Separator orientation="vertical" className="h-6" />
+
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+          <NavLink to="/">
+            <Home size={20} />
+            Início
+          </NavLink>
+
+          <NavLink to="/orders">
+            <UtensilsCrossed size={20} />
+            Pedidos
+          </NavLink>
+        </nav>
+      </div>
     </header>
   )
 }
