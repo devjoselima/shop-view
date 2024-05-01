33599d058d9f22983924ff6d5533703c528f39cb
Jose Lima <jalimag0206@gmail.com>
2024-05-01 16:58:22 -0300

    feat: add react helmet SEO

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,7 +1,10 @@
+import { Helmet } from 'react-helmet-async'
+
 export const SignIn = () => {
   return (
-    <div>
+    <>
+      <Helmet title="Login" />
       <h1>SignIn</h1>
-    </div>
+    </>
   )
 }
