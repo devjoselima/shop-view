9b69dfbbac1d8e256151f110011f57b237c2175a
Jose Lima <jalimag0206@gmail.com>
2024-05-01 18:57:00 -0300

    refactor: adjust folders

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom'
 
-import { AppLayout } from './pages/_layouts/app'
+import { AppLayout } from './pages/_layouts/appLayout'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Dashboard } from './pages/app/dashboard'
 import { SignIn } from './pages/auth/sign-in'
