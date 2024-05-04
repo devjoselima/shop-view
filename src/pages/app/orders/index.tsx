b3edf97d0a6fd3f5634a563286e71c0231c630a2
Jose Lima <jalimag0206@gmail.com>
2024-05-04 15:39:24 -0300

    feat: add Pagination

--- src/pages/app/orders/index.tsx
+++ src/pages/app/orders/index.tsx
@@ -1,5 +1,6 @@
 import { Helmet } from 'react-helmet-async'
 
+import { Pagination } from '@/components/pagination'
 import { Table, TableBody } from '@/components/ui'
 
 import { OrderTableFilters, OrderTableHead, OrderTableRow } from './components'
@@ -10,20 +11,22 @@ export const Orders = () => {
       <Helmet title="Pedidos" />
       <div className="flex flex-col gap-4">
         <h1 className="tracking-light text-3xl font-bold">Pedidos</h1>
-      </div>
-      <div className="5 flex flex-col gap-1 space-y-2">
-        <OrderTableFilters />
+        <div className="5 flex flex-col gap-1 space-y-2">
+          <OrderTableFilters />
 
-        <div className="rounded-md border">
-          <Table>
-            <OrderTableHead />
+          <div className="rounded-md border">
+            <Table>
+              <OrderTableHead />
 
-            <TableBody>
-              {Array.from({ length: 10 }).map((_, i) => {
-                return <OrderTableRow key={i} />
-              })}
-            </TableBody>
-          </Table>
+              <TableBody>
+                {Array.from({ length: 10 }).map((_, i) => {
+                  return <OrderTableRow key={i} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
       </div>
     </>
