41e368cbd2c9a60efa0cc2e14e618476c988d002
Jose Lima <jalimag0206@gmail.com>
2024-05-26 18:49:10 -0300

    chore: add 'mock' sufix in mocks files

--- src/api/mocks/index.ts
+++ src/api/mocks/index.ts
@@ -2,17 +2,17 @@ import { setupWorker } from 'msw/browser'
 
 import { env } from '@/env'
 
-import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period'
-import { getDayOrdersAmountMock } from './get-day-orders-amount'
-import { getManagedRestaurantMock } from './get-managed-restaurants'
-import { getMonthCanceledAmountMock } from './get-month-canceled-orders-amount'
-import { getMonthOrdersAmountMock } from './get-month-orders-amount'
-import { getMonthRevenueMock } from './get-month-revenue'
-import { getPopularProductsMock } from './get-popular-products'
-import { getProfileMock } from './get-profile'
+import { getDailyRevenueInPeriodMock } from './get-daily-revenue-in-period-mock'
+import { getDayOrdersAmountMock } from './get-day-orders-amount-mock'
+import { getManagedRestaurantMock } from './get-managed-restaurants-mock'
+import { getMonthCanceledAmountMock } from './get-month-canceled-orders-amount-mock'
+import { getMonthOrdersAmountMock } from './get-month-orders-amount-mock'
+import { getMonthRevenueMock } from './get-month-revenue-mock'
+import { getPopularProductsMock } from './get-popular-products-mock'
+import { getProfileMock } from './get-profile-mock'
 import { signInMock } from './sign-in-mock'
 import { signUpMock } from './sign-up-mock'
-import { updateProfileMock } from './update-profile'
+import { updateProfileMock } from './update-profile-mock'
 
 export const worker = setupWorker(
   signInMock,
